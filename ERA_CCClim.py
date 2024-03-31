991b0d83c716fe58c14e109be01a3e65a8b44307
Arndt Kaps <arndt.kaps@dlr.de>
2024-03-31 12:07:16 +0200

    forgot a change in previous commit

--- ERA_CCClim.py
+++ ERA_CCClim.py
@@ -16,6 +16,7 @@ if __name__=="__main__":
     wap_files = glob.glob(os.path.join(scratch,"ERA5/wap/*"))
     ts_files = glob.glob(os.path.join(scratch,"ERA5/ts/*"))
     ctnames = ["Ci","As","Ac","St","Sc","Cu","Ns","Dc"]
+    spec = "cropped" 
     
     if not os.path.exists(os.path.join(scratch,"ERA5.parquet")):
         print("making new parquet file")
@@ -29,18 +30,18 @@ if __name__=="__main__":
         print("comparing ERA and CCClim")
         era_df = pd.read_parquet(os.path.join(scratch,"ERA5.parquet"))
         
-        compare = compare_era(  era_df, "month")
+        compare = compare_era(  era_df, "month",spec)
         pool=Pool(10)
 
         joints = pool.map(compare,np.arange(1983,2016))
         joints =pd.concat(joints).reset_index()
         joints_land = joints.loc[globe.is_land(joints.lat,joints.lon),:]
         joints_land = joints_land.set_index(["lat","lon","month"])
-        joints_land.to_parquet(os.path.join(scratch,"reanalysis_land.parquet"))
+        joints_land.to_parquet(os.path.join(scratch,"reanalysis_land{}.parquet".format(spec)))
         print(len(joints), len(joints_land))
         del joints_land
         joints_ocean = joints.loc[globe.is_ocean(joints.lat,joints.lon),:]
         joints_ocean = joints_ocean.set_index(["lat","lon","month"])
         print(len(joints), len(joints_ocean))
-        joints_ocean.to_parquet(os.path.join(scratch,"reanalysis_ocean.parquet"))
+        joints_ocean.to_parquet(os.path.join(scratch,"reanalysis_ocean{}.parquet".format(spec)))
             
